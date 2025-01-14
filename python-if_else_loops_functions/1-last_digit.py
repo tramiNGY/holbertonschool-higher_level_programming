bbf372b71f3c52dee3d6428ef7eca928da2a334b
tramiNGY <ngytrami@gmail.com>
2025-01-14 18:08:16 +0100

    Added 1-last_digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastdigit = number % 10
+isgreater = "is greater than 5"
+isless = "is less than 6 and not 0"
+if number >= 0:
+    if lastdigit > 5:
+        print("Last digit of", number, "is", lastdigit, isgreater)
+    elif lastdigit == 0:
+        print("Last digit of", number, "is", lastdigit, "and is 0")
+    elif 0 < lastdigit < 6:
+        print("Last digit of", number, "is", lastdigit, isless)
+else:
+    if lastdigit == 0:
+        print("Last digit of", number, "is", lastdigit, "and is 0")
+    else:
+        print("Last digit of", number, "is", - (10 - lastdigit), isless)
