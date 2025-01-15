44bc33ac9c0e18496f8dc43b60bdc469598d4873
tramiNGY <ngytrami@gmail.com>
2025-01-15 18:16:26 +0100

    Added 8-uppercase using chr and ord function

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upperstring = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            upperstring += chr(ord(i) - 32)
+        else:
+            upperstring += i
+    print(upperstring)
