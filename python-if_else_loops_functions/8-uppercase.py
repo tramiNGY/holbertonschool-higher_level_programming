cee7f11c1ba9c89d3a8da3f1b489a317a25d5057
tramiNGY <ngytrami@gmail.com>
2025-01-15 18:48:47 +0100

    Requirement was not to not use any variable so returned to former code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(''.join([chr(ord(i) - 32) if 97 <= ord(i) <= 122
-                   else i for i in str]))
+    upperstring = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            upperstring += chr(ord(i) - 32)
+        else:
+            upperstring += i
+    print(upperstring)
