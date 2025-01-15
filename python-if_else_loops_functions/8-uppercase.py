61ec4bf906715e3d2ff74e2cfe3bf70cd95f491a
tramiNGY <ngytrami@gmail.com>
2025-01-15 18:58:46 +0100

    Not list type requirement so back to original code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upperstring = []
+    upperstring = ""
     for i in str:
         if 97 <= ord(i) <= 122:
-            upperstring.append(chr(ord(i) - 32))
+            upperstring += chr(ord(i) - 32)
         else:
-            upperstring.append(i)
-    print(''.join(upperstring))
+            upperstring += i
+    print(upperstring)
