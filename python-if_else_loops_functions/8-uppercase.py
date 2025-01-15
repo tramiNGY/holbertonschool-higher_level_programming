278aaf53328f312b737bc69fd4360d2d74d806c8
tramiNGY <ngytrami@gmail.com>
2025-01-15 18:43:24 +0100

    Used join function to not store any character in a variable

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upperstring = ""
-    for i in str:
-        if 97 <= ord(i) <= 122:
-            upperstring += chr(ord(i) - 32)
-        else:
-            upperstring += i
-    print(upperstring)
+    print(''.join([chr(ord(i) - 32) if 97 <= ord(i) <= 122
+                   else i for i in str]))
