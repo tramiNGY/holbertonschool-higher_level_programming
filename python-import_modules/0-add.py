395b2287aa94869413aa72bb21d0dbf4a1cf364a
tramiNGY <ngytrami@gmail.com>
2025-01-16 17:18:21 +0100

    Renamed name module for requirement only use word add_0 once

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    import add_0 as addition
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, addition.add(a, b)))
