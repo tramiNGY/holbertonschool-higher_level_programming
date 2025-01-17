d12ab24784f7db90b62a07ead0040f26814fe7ac
tramiNGY <ngytrami@gmail.com>
2025-01-17 12:03:43 +0100

    Added 2-args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) - 1 == 0:
+    print("{} arguments.".format(len(sys.argv) - 1))
+elif len(sys.argv) - 1 == 1:
+    print("{} argument:".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+
+for i in range(1, len(sys.argv)):
+    print("{}: {}".format(i, sys.argv[i]))
