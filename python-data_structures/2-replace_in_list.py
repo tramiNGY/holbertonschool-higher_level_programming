1084dc1eac0b6e1c215cce85ab7d21e154517a6a
tramiNGY <ngytrami@gmail.com>
2025-01-20 11:50:09 +0100

    If out of range should return original list and not none

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     if idx > len(my_list) - 1:
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
