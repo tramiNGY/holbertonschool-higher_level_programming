a33d0b1f65dcaf5ae63587c0ed5c06c0aaec7c10
tramiNGY <ngytrami@gmail.com>
2025-01-20 13:33:20 +0100

    Modified return to copy of list instead of original

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx > len(my_list) - 1:
-        return my_list
     my_list_replaced = my_list[:]
+    if idx < 0:
+        return my_list_replaced
+    if idx > len(my_list) - 1:
+        return my_list_replaced
     my_list_replaced[idx] = element
     return my_list_replaced
