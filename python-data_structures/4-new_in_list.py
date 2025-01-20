c32fbae4f5ad4cd806ad21a9ed149678441fe841
tramiNGY <ngytrami@gmail.com>
2025-01-20 13:39:27 +0100

    Forgot to add shebang, put first method back

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list_replaced = my_list.copy()
+    my_list_replaced = my_list[:]
     if idx < 0:
         return my_list_replaced
     if idx > len(my_list) - 1:
