20e5885e74c539b33e1b98371f22b308412b60f3
tramiNGY <ngytrami@gmail.com>
2025-01-20 11:22:06 +0100

    Misread out of range for number of elements in list not in value

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx <= 0:
         return None
     for i in my_list:
-        if i > idx:
+        if idx > len(my_list):
             return None
         else:
             return my_list[idx]
