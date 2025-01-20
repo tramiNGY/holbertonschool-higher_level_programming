5b1554ea54b19b26443fa38ce0b0d879c9c0b31d
tramiNGY <ngytrami@gmail.com>
2025-01-20 21:52:49 +0100

    Changed initialization of max to first element of the list instead of 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        max = 0
-        for i in my_list:
-            if i >= max:
+        max = my_list[0]
+        for i in my_list[1:]:
+            if i > max:
                 max = i
     return max
