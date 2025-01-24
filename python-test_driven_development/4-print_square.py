1357c55a055c55079695a477737e87286b4a2dff
tramiNGY <ngytrami@gmail.com>
2025-01-24 15:28:23 +0100

    Added 4-print_square with exceptions TypeError and ValueError with pycodestyle checked

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+The 4-print_squares module supplies one function print_square.
+It permits to print size square of #.
+size is an integer or raise TypeError and positive or raise ValueError.
+'''
+
+
+def print_square(size):
+    '''
+    prints a square with the character #
+    '''
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        if isinstance(size, float):
+            raise TypeError("size must be an integer")
+        else:
+            raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
