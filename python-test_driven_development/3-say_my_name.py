cee44d4e19040ef454f4c8d9e12c6dc47acb95d9
tramiNGY <ngytrami@gmail.com>
2025-01-24 14:57:27 +0100

    Added condition where no arguments passed ie None

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,12 +10,13 @@ def say_my_name(first_name, last_name=""):
     '''
     Prints the first name and last name
     '''
+    if first_name is None:
+        raise TypeError("first_name must be a string")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    if last_name == "":
+        print("My name is {} ".format(first_name))
     else:
-        if last_name == "":
-            print("My name is {} ".format(first_name))
-        else:
-            print("My name is {} {}".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
