91a6c33b9f1603b369cabbdb1e016b0bc3aebadc
tramiNGY <ngytrami@gmail.com>
2025-01-24 21:01:44 +0100

    Modified first condition if first_name is None only and added default value of None if no argument passed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,11 +6,11 @@ It should permit to print only one name is the other ommited.
 '''
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name=None, last_name=""):
     '''
     Prints the first name and last name
     '''
-    if first_name is None and last_name is None:
+    if first_name is None:
         raise TypeError("first_name must be a string")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
