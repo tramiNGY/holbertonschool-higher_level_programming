010422078a0995d27656142abfe94d0e0e671ac1
tramiNGY <ngytrami@gmail.com>
2025-01-28 11:11:07 +0100

    Mistype first exception error message forgot t at must word

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     """This class defines a square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("size mus be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise TypeError("size must be >= 0")
         else:
