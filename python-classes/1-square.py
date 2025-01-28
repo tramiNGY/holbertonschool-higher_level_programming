5bf68c64c3bcfb76102d3286a3a87d9f538c787e
tramiNGY <ngytrami@gmail.com>
2025-01-28 11:04:38 +0100

    Added 1-square with a private instance attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+This module contains one class Square
+'''
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size):
+        self.__size = size
