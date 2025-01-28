4894ffe1592b0143d00f46df72a55e2367b997d2
tramiNGY <ngytrami@gmail.com>
2025-01-28 12:13:07 +0100

    Corrected second exception ValueError and not TypeError

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,6 +10,6 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise TypeError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
