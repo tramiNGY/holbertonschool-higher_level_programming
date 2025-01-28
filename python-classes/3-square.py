a76dbbb8c6a1cca46b1eaf825640894ba2234783
tramiNGY <ngytrami@gmail.com>
2025-01-28 11:27:44 +0100

    Added 3-square with new public method area using private instance variable from init method to calculate area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+This module contains one class Square
+'''
+
+
+class Square:
+    """This class defines a Square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
