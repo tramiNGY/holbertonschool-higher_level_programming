87fb6868f50858f61d5cf799a62c733eb2b32a38
tramiNGY <ngytrami@gmail.com>
2025-02-03 17:04:08 +0100

    Added docstyle for function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,9 @@ This module contains one function is_same_class
 
 
 def is_same_class(obj, a_class):
+    '''
+    Checks if obj is exactly of a_class
+    '''
     if type(obj) is a_class:
         return True
     else:
