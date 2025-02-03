895a9e6bb06a846ccc7896f7e8d6db2c8f28ec0f
tramiNGY <ngytrami@gmail.com>
2025-02-03 17:02:11 +0100

    Added 2-is_same using type

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+This module contains one function is_same_class
+'''
+
+
+def is_same_class(obj, a_class):
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
