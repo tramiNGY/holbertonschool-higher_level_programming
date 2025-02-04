0812248a5c99a83a4fb55b566f983c91edee7655
tramiNGY <ngytrami@gmail.com>
2025-02-04 13:40:30 +0100

    Added new class Square inherited from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+This module contains one class BaseGeometry and inherited class Rectangle
+'''
+
+
+class BaseGeometry:
+    """Class for basic geometry calculation"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Inherited class of BaseGeometry"""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """Inherited class of Rectangle"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
