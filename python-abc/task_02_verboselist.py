b552db793f2c56760727604c9be00ffcfe9b99ae
tramiNGY <ngytrami@gmail.com>
2025-02-04 17:40:01 +0100

    Created Inherited class from list built-in and extended the class modiying methods append extend remove and pop

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This module contains one class VerboseList
+'''
+
+
+class VerboseList(list):
+    """Inherited class from list built-in class"""
+    def append(self, item):
+        super().append(item)
+        print(f"Added {item} to the list.")
+
+    def extend(self, x):
+        super().extend(x)
+        print(f"Extended the list with {x} items.")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed {item} from the list.")
+
+    def pop(self, index=-1):
+        popped_item = super().pop(index)
+        print(f"Popped {popped_item} from the list.")
