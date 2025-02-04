48adef3c98a22e9ea7a58a1e8e79d564fdb3e55d
tramiNGY <ngytrami@gmail.com>
2025-02-04 10:29:59 +0100

    Added area method raising Exception and main

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+This module only contains one class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    """Class for basic geometry calculation"""
+    def area(self):
+        raise Exception("area() is not implemented")
