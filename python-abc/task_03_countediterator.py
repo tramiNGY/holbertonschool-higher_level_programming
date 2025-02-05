683146f26595dd321f18c895b42d317c9a2beeab
tramiNGY <ngytrami@gmail.com>
2025-02-05 12:35:49 +0100

    Created class using iter and __next__ method

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+This module contains one class CountedIterator
+'''
+
+
+class CountedIterator:
+    """Extends the built-in iterator"""
+    def __init__(self, some_iterable, count=0):
+        self.__some_iterable = some_iterable
+        self.__iterator = iter(some_iterable)
+        self.__count = count
+
+    def get_count(self):
+        return self.__count
+
+    def __next__(self):
+        self.__count += 1
+        if self.__count > len(self.__some_iterable):
+            raise StopIteration
+        else:
+            return next(self.__iterator)
