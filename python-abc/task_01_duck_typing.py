3ac5426ad55b34556f12afb68914eda63da1e613
tramiNGY <ngytrami@gmail.com>
2025-02-06 11:14:32 +0100

    Changed abs function to perimeter

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -23,10 +23,10 @@ class Circle(Shape):
         self.__radius = radius
 
     def area(self):
-        return math.pi * abs(self.__radius) ** 2
+        return math.pi * self.__radius ** 2
 
     def perimeter(self):
-        return 2 * math.pi * self.__radius
+        return 2 * math.pi * abs(self.__radius)
 
 
 class Rectangle(Shape):
