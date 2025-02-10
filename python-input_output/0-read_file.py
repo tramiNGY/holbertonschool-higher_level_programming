85078b8031777ad23a551288b9d9f4d252355caa
tramiNGY <ngytrami@gmail.com>
2025-02-10 14:25:13 +0100

    Added function description

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ This module contains one function read_files
 
 
 def read_file(filename=""):
+    """Opening file and reading it before printing content""" 
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
     print(read_data, end='')
