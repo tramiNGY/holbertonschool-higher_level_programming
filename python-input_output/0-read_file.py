49c2867c3b5f93e00aef96cf4bcf1eed0d1716bf
tramiNGY <ngytrami@gmail.com>
2025-02-10 14:24:09 +0100

    Added r for read in open function and put print outside open block

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ This module contains one function read_files
 
 
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, end='')
+    print(read_data, end='')
