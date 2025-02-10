5c6071c692920fd2ef8dc05cfe6c71e704479158
tramiNGY <ngytrami@gmail.com>
2025-02-10 15:28:45 +0100

    create obj from json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+This module contains one function load_from_json_file
+'''
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a json file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        obj_str = f.read()
+    print(obj_str)
