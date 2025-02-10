4dc5b473bfe9e7210dab79bf0ce7c1b137cd6c6b
tramiNGY <ngytrami@gmail.com>
2025-02-10 14:21:09 +0100

    Used read fucntion

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+This module contains one function read_files
+'''
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
