7becee115aedde644dc69a4f2d1c7edf67037cd8
tramiNGY <ngytrami@gmail.com>
2025-02-10 15:20:01 +0100

    changed dumps to dump

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,6 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Open filename, converts m_obj and writes it in file"""
-    json_obj = json.dumps(my_obj)
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(json_obj)
+        json.dump(my_obj, f)
