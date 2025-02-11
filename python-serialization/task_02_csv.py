b21827e0a4db615999d118ef9184d9d26f256696
tramiNGY <ngytrami@gmail.com>
2025-02-11 17:27:27 +0100

    converted csv to json Using reader, dictreader and dump

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+
+'''
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """converts Comma Seperated Value to JSON format"""
+    data = []
+    with open(csv_filename, 'r', encoding="utf-8") as cvsf:
+        cvsReader = csv.DictReader(cvsf)
+        for rows in cvsReader:
+            data.append(rows)
+
+    with open("data.json", 'w', encoding="utf-8") as jsonf:
+        json.dump(data, jsonf)
