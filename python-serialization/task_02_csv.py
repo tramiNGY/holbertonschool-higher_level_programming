debcca10455954ded7934709e60e1500b5acd0ad
tramiNGY <ngytrami@gmail.com>
2025-02-11 17:31:47 +0100

    Forgot requirement return true if conversion succeeded and false if exception

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,11 +8,17 @@ import json
 
 def convert_csv_to_json(csv_filename):
     """converts Comma Seperated Value to JSON format"""
-    data = []
-    with open(csv_filename, 'r', encoding="utf-8") as cvsf:
-        cvsReader = csv.DictReader(cvsf)
-        for rows in cvsReader:
-            data.append(rows)
+    try:
+        data = []
+        with open(csv_filename, 'r', encoding="utf-8") as cvsf:
+            cvsReader = csv.DictReader(cvsf)
+            for rows in cvsReader:
+                data.append(rows)
 
-    with open("data.json", 'w', encoding="utf-8") as jsonf:
-        json.dump(data, jsonf)
+        with open("data.json", 'w', encoding="utf-8") as jsonf:
+            json.dump(data, jsonf)
+
+        return True
+
+    except Exception:
+        return False
