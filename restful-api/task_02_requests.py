0522aa9e4ff22d1223a9844df18db9f8743a4ecd
tramiNGY <ngytrami@gmail.com>
2025-02-18 20:21:16 +0100

    In fetch and save function removed print and put with open in if block

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -20,14 +20,11 @@ def fetch_and_save_posts():
     """saves posts into csv file"""
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     if r.status_code == 200:
-        print(f"Status Code: {r.status_code}")
         posts = r.json()
-        for post in posts:
-            print(post['title'])
 
-    with open('posts.csv', mode='w', newline='', encoding="utf-8") as f:
-        keys = ["id", "title", "body"]
-        writer = csv.DictWriter(f, fieldnames=keys)
-        writer.writeheader()
-        for post in posts:
-            writer.writerow({key: post[key] for key in keys})
+        with open('posts.csv', mode='w', newline='', encoding="utf-8") as f:
+            keys = ["id", "title", "body"]
+            writer = csv.DictWriter(f, fieldnames=keys)
+            writer.writeheader()
+            for post in posts:
+                writer.writerow({key: post[key] for key in keys})
