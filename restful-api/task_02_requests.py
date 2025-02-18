87e1cfef9a1b32f62b7f882e754a016f1117beab
tramiNGY <ngytrami@gmail.com>
2025-02-18 18:14:42 +0100

    Added try except

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,26 +8,34 @@ import csv
 
 def fetch_and_print_posts():
     """fetches all posts and print status code and titles"""
-    r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f"Status Code: {r.status_code}")
-    if r.status_code == 200:
-        posts = r.json()
-        for post in posts:
-            print(post['title'])
+    try:
+        r = requests.get('https://jsonplaceholder.typicode.com/posts')
+        print(f"Status Code: {r.status_code}")
+        if r.status_code == 200:
+            posts = r.json()
+            for post in posts:
+                print(post['title'])
+                
+    except requests.exceptions:
+        return
 
 
 def fetch_and_save_posts():
     """saves posts into csv file"""
-    r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f"Status Code: {r.status_code}")
-    if r.status_code == 200:
-        posts = r.json()
-        for post in posts:
-            print(post['title'])
+    try:
+        r = requests.get('https://jsonplaceholder.typicode.com/posts')
+        print(f"Status Code: {r.status_code}")
+        if r.status_code == 200:
+            posts = r.json()
+            for post in posts:
+                print(post['title'])
 
-    with open('posts.csv', mode='w', newline='', encoding="utf-8") as f:
-        keys = ["id", "title", "body"]
-        writer = csv.DictWriter(f, fieldnames=keys)
-        writer.writeheader()
-        for post in posts:
-            writer.writerow({key: post[key] for key in keys})
+        with open('posts.csv', mode='w', newline='', encoding="utf-8") as f:
+            keys = ["id", "title", "body"]
+            writer = csv.DictWriter(f, fieldnames=keys)
+            writer.writeheader()
+            for post in posts:
+                writer.writerow({key: post[key] for key in keys})
+
+    except requests.exceptions:
+        return
