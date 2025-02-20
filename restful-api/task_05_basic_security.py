14bcf10c4c891a03fd2c0842eb027486d8980858
tramiNGY <ngytrami@gmail.com>
2025-02-20 17:00:52 +0100

    added Error Handlers

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -62,5 +62,30 @@ def admin_only():
     return "Admin Access: Granted"
 
 
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
+
+
 if __name__ == '__main__':
     app.run(debug=True)
