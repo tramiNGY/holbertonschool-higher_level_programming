65fb7cb3bb7f2cd851b0f8b940954ae237e35149
tramiNGY <ngytrami@gmail.com>
2025-02-21 10:58:54 +0100

    Added return None in verify_password

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -29,6 +29,7 @@ def verify_password(username, password):
     user = users.get(username)
     if user and check_password_hash(user['password'], password):
         return user
+    return None
 
 
 @app.route('/basic-protected')
