a2e77e420dbfa34e097ab7e366e0ae5691d19b35
tramiNGY <ngytrami@gmail.com>
2025-03-05 19:40:49 +0100

    put back alias name

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name AS city_name, states.name AS state_name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id AS id, cities.name AS name, states.name AS name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
