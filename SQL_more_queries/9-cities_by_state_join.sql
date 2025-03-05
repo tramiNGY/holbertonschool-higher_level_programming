bc86eca714cb58170c0a142f2295ffd324d9afd6
tramiNGY <ngytrami@gmail.com>
2025-03-05 19:34:31 +0100

    Added comma and city_name state_name

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id AS id cities.name AS name states.name AS name FROM cities JOIN states ON cities.states_id = state_id ORDER BY cities.id ASC;
+SELECT cities.id AS id, cities.name AS city_name, states.name AS state_name FROM cities JOIN states ON cities.states_id = state_id ORDER BY cities.id ASC;
