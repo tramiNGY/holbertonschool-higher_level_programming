65c20a6d01f13b4424fcbffca3a6af6a7b380be2
tramiNGY <ngytrami@gmail.com>
2025-03-07 16:15:51 +0100

    displays where name match

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+This module takes in an argument
+and displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name = '{}' "
+                   "ORDER BY id ASC".format(state_name))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
