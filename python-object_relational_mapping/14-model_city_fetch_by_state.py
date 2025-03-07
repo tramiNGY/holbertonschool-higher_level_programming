560042aafc9dc2e3e1e42514e19d593f3bdefb68
tramiNGY <ngytrami@gmail.com>
2025-03-07 18:00:05 +0100

    pycode

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,6 +3,8 @@
 from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+
+
 class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
