654b0607e6c6281f8c053ebf09a13092dff8e9eb
tramiNGY <ngytrami@gmail.com>
2025-03-07 18:12:00 +0100

    pycode

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python3
-''' model city fetch by state '''
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""List all City objects from the database hbtn_0e_14_usa"""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from model_city import City  # N'oubliez pas d'importer la classe City !
 
-class City(Base):
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+if __name__ == "__main__":
+
+    # Vérification du nombre d'arguments
+    if len(sys.argv) != 4:
+        exit(1)
+
+    # Connexion à la base de données
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, db_name),
+        pool_pre_ping=True
+    )
+
+    # Création de la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Interroger les villes et les états associés
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+
+    # Affichage des résultats
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    # Fermeture de la session
+    session.close()
