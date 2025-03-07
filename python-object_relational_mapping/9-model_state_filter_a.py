207dd4cde3370dd34d8f094d4175f33b3a0b3345
tramiNGY <ngytrami@gmail.com>
2025-03-07 17:23:44 +0100

    nothing display check

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,9 +15,11 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, db_name),
+        pool_pre_ping=True
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -25,7 +27,10 @@ if __name__ == "__main__":
     states = session.query(State).filter(State.name.like('%a%')) \
         .order_by(State.id).all()
 
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    if not states:
+        print("Nothing")
+    else:
+        for state in states:
+            print(f"{state.id}: {state.name}")
 
     session.close()
