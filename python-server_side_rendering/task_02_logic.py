a31429ce85cc12b69b57d3c41449dd65529be69a
tramiNGY <ngytrami@gmail.com>
2025-03-21 17:14:47 +0100

    put back previous version

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -18,12 +18,9 @@ def contact():
 
 @app.route('/items')
 def items():
-    try:
-        with open('items.json') as f:
-            data = json.load(f)
-        return render_template('items.html', items=data['items'])
-    except Exception:
-        return "No items found"
+    with open('items.json') as f:
+        data = json.load(f)
+    return render_template('items.html', items=data['items'])
 
 if __name__ == '__main__':
        app.run(debug=True, port=5000)
