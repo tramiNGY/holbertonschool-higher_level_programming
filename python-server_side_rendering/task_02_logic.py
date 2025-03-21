c77a744e709e05e0970122da11681fbd2de05f20
tramiNGY <ngytrami@gmail.com>
2025-03-21 17:09:40 +0100

    Added try except

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -18,9 +18,12 @@ def contact():
 
 @app.route('/items')
 def items():
-    with open('items.json') as f:
-        data = json.load(f)
-    return render_template('items.html', items=data['items'])
+    try:
+        with open('items.json') as f:
+            data = json.load(f)
+        return render_template('items.html', items=data['items'])
+    except Exception:
+        return "Error"
 
 if __name__ == '__main__':
        app.run(debug=True, port=5000)
