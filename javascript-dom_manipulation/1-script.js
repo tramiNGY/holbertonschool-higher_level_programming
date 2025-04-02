30ec5318c69b1703de49a9160bc01836da16a45b
tramiNGY <ngytrami@gmail.com>
2025-04-02 10:41:34 +0200

    querySelector on id element and addEventListener

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header');
+const red_header = document.querySelector('#red_header');
+
+red_header.addEventListener('click', () => {
+  header.style.color = '#FF0000';
+});
